e5b444dc00c28c47b4756de6545a85502f270bc9
labr <labr@paypal.com>
2016-01-31 12:22:16 +0530

    :gem: basic version

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,11 +1,11 @@
+<?php
 $errors = '';
-// $myemail = 'manikhandhan@rmgmachines.com';
-$myemail = 'brlalithkumar@gmail.com';
+$myemail = 'manikhandhan@rmgmachines.com';
 
 if(empty($_POST['name'])  ||
    empty($_POST['phonenumber']) ||
    empty($_POST['companyname']) ||
-   empty($_POST['adress']) ||
+   empty($_POST['address']) ||
    empty($_POST['message']))
 {
     $errors .= "\n Error: all fields are required";
@@ -26,17 +26,22 @@ $email_address))
 
 if( empty($errors))
 {
-$to = $myemail;
-$email_subject = "Enquiry for RMG Machines from $name";
-$email_body = "You have received a new message.".
-" Here are the details:\n Name: $name \n ".
-"Phone Number: $phonenumber\n".
-"Company Name: $companyname\n".
-"Address: $address\n".
-"Message: $message\n";
-$headers = "From: $myemail\n";
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-//redirect to the 'thank you' page
-header('Location: contact-form-thank-you.html');
+  $to = $myemail;
+  $email_subject = "Enquiry for RMG Machines from $name";
+  $email_body = "You have received a new message.".
+  " Here are the details:\n Name: $name \n ".
+  "Phone Number: $phonenumber\n".
+  "Company Name: $companyname\n".
+  "Address: $address\n".
+  "Message: $message\n";
+  $headers = "From: $myemail\n";
+  $headers .= "Reply-To: $email_address";
+  mail($to,$email_subject,$email_body,$headers);
+  //redirect to the 'thank you' page
+  header('Location: contact-form-thank-you.html');
 }
+else {
+  // echo $errors;
+  header('Location: contact-form-error.html');
+}
+?>
